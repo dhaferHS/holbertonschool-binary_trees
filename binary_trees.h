862f4f9f8ae986905ef0b33ba6dca3233f17421c
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-02 12:52:52 +0100

    creat a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -24,9 +24,9 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
-typedef struct binary_tree_s bst_t; //Binary Search Tree//
-typedef struct binary_tree_s avl_t; //AVL Tree//
-typedef struct binary_tree_s heap_t; //Max Binary Heap//
+typedef struct binary_tree_s bst_t; /**Binary Search Tree*/
+typedef struct binary_tree_s avl_t; /**AVL Tree*/
+typedef struct binary_tree_s heap_t; /**Max Binary Heap*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);//task 0//
 #endif
