c0f8a5ea46893f2161854d522681441a190bb807
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-03 17:38:20 +0100

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -34,4 +34,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);/**tas
 void binary_tree_delete(binary_tree_t *tree);/**task3**/
 int binary_tree_is_leaf(const binary_tree_t *node);/**task4**/
 int binary_tree_is_root(const binary_tree_t *node);/**task5**/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));/**task6**/
 #endif
