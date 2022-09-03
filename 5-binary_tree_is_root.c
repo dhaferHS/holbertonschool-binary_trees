48c1f657c9caab762f0e2ebb17e0942dbeae3846
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-03 17:08:29 +0100

    5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - binary_tree_is_root
+ * @node: pointer to the node to check
+ * 
+ * Return: 1 or 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (node == NULL)
+	return (0);
+
+if (!node->parent)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
