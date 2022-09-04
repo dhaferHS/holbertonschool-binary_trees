db20908f575f81c541b7632020b7371061a08ad2
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-04 16:04:07 +0100

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree:  pointer to the root node of the tree to count the number of nodes
- * 
+ *
  * Return:  number of nodes or NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
