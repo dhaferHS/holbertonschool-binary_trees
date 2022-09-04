c04563ea65fc063d5560e10fa964a4fa4b0b3566
dhaferHS <dhafer.darkprod@gmail.com>
2022-09-04 14:49:43 +0100

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: pointer to the root node of the tree to measure the size
- * 
+ *
  * Return: size or NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
@@ -11,7 +11,7 @@ if (tree == NULL)
 {
 	return (0);
 }
-else 
+else
 {
 return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1 );
 }
